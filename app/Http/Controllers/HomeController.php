1b82affdda9621ee65853e23f95395611ecc5912
hafizal hussin <hafizalhussin@hafizals-MacBook-Pro.local>
2020-02-25 18:22:41 +0800

    new:tutorial 1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,8 @@ class HomeController extends Controller
      */
     public function index()
     {
+        //Role::create(['name'=>'writer']);
+        Permission::create(['name'=> 'write post']);
         return view('home');
     }
 }
